ad2f72d77a4f4d1ea423d36e2bed3a7793877011
Sarah Kirk <kirkse710@gmail.com>
2018-07-30 19:35:14 -0700

    begins leap year exercise

--- leap.py
+++ leap.py
@@ -1,2 +1,13 @@
 def is_leap_year(year):
-    pass
+    if year % 100 == 0 and year % 400 == 0:
+        return True
+    if year % 4 == 0 and year % 400 == 0:
+        return True
+    elif year % 100 == 0 and year % 4 == 0:
+        return False
+    else:
+        return False
+
+# on every year that is evenly divisible by 4
+#   except every year that is evenly divisible by 100
+#     unless the year is also evenly divisible by 400
