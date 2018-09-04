0ce3e2816660331060b2e90c5836baae28d3edea
Sarah Kirk <kirkse710@gmail.com>
2018-09-04 09:42:09 -0700

    tooling leap year

--- leap.py
+++ leap.py
@@ -1,5 +1,5 @@
 def is_leap_year(year):
-    if year_divisible_by_four(year)
+    if year_divisible_by_four(year):
     # if year % 100 == 0 and year % 400 == 0:
     #     return True
     # if year % 4 == 0 and year % 400 == 0:
@@ -14,7 +14,11 @@ def year_divisible_by_four(year):
         return True
 
 def year_divisible_by_100(year):
-    
+    if year % 400 == 0 and year % 100 == 0:
+        return True
+    if year % 100 == 0 and year % 400 != 0:
+        return False 
+
 
 # on every year that is evenly divisible by 4
 #   except every year that is evenly divisible by 100
