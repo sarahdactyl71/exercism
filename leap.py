e193d215f29286ce9c6100bcf242c63b9925c236
Sarah Kirk <kirkse710@gmail.com>
2018-09-05 11:07:47 -0700

    additional refactoring in leap.py

--- leap.py
+++ leap.py
@@ -1,7 +1,5 @@
 def is_leap_year(year):
-    if year % 4 == 0 and year % 100 != 0:
-        return True
-    elif year % 400 == 0 and year % 100 == 0 and year % 4 == 0:
+    if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
         return True
     else:
         return False
